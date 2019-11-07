6f765e3fa488c402a2dbddd83afd1dfaef4c4d7c
otajisan <taji.taji.otajisan@gmail.com>
2019-11-08 08:20:23 +0900

    :tada: initial commit

---
+++ src/test/kotlin/io/code/morning/api/ApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.code.morning.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
