af07b0c028b19ace009af6324c7425bae4937173
otajisan <taji.taji.otajisan@gmail.com>
2020-05-20 09:37:26 +0900

    :+1: modified

--- src/main/kotlin/io/code/morning/api/controller/IndexController.kt
+++ src/main/kotlin/io/code/morning/api/controller/IndexController.kt
@@ -1,6 +1,8 @@
 package io.code.morning.api.controller
 
+import io.code.morning.api.controller.resource.BlogPageResource
 import io.code.morning.api.controller.resource.IndexResource
+import io.code.morning.api.usecase.BlogUsecase
 import io.code.morning.api.usecase.IndexUsecaseImpl
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.responses.ApiResponse
@@ -12,38 +14,38 @@ import org.springframework.web.bind.annotation.*
 import reactor.core.publisher.Mono
 
 @RestController
-@RequestMapping("/root")
+@RequestMapping("/")
 class IndexController(
-        private val indexService: IndexUsecaseImpl
+    private val indexService: IndexUsecaseImpl,
+    private val blogUsecase: BlogUsecase
 ) {
 
-    @Operation(summary = "root api")
-    @ApiResponses(value = [
-        ApiResponse(responseCode = "200", description = "Success"),
-        ApiResponse(responseCode = "400", description = "Bad request"),
-        ApiResponse(responseCode = "401", description = "Unauthorized"),
-        ApiResponse(responseCode = "500", description = "Internal server error")
-    ])
-    @ResponseStatus(HttpStatus.OK)
-    @GetMapping(value = [""])
-    suspend fun get(): ResponseEntity<IndexResource> {
+  @Operation(summary = "root api")
+  @ApiResponses(value = [
+    ApiResponse(responseCode = "200", description = "Success"),
+    ApiResponse(responseCode = "400", description = "Bad request"),
+    ApiResponse(responseCode = "401", description = "Unauthorized"),
+    ApiResponse(responseCode = "500", description = "Internal server error")
+  ])
+  @ResponseStatus(HttpStatus.OK)
+  @GetMapping(value = [""])
+  suspend fun get(): ResponseEntity<IndexResource> {
+    val blogs = blogUsecase.list(0, 10).awaitSingle()
+    val resource = IndexResource(
+        message = "MorningCode.io",
+        blogs = blogs
+    )
+    return ResponseEntity(resource, HttpStatus.OK)
+  }
 
-        val resource = IndexResource(
-                message = "Hello Morning Code API.",
-                blogs = indexService.makePageResponse()
-        )
-
-        return ResponseEntity(resource, HttpStatus.OK)
-    }
-
-    @Operation(summary = "ping")
-    @ApiResponses(value = [
-        ApiResponse(responseCode = "200", description = "Success"),
-        ApiResponse(responseCode = "400", description = "Bad request"),
-        ApiResponse(responseCode = "401", description = "Unauthorized"),
-        ApiResponse(responseCode = "500", description = "Internal server error")
-    ])
-    @ResponseStatus(HttpStatus.CREATED)
-    @GetMapping(value = ["/ping"])
-    suspend fun ping(): String = Mono.just("pong").awaitSingle()
+  @Operation(summary = "ping")
+  @ApiResponses(value = [
+    ApiResponse(responseCode = "200", description = "Success"),
+    ApiResponse(responseCode = "400", description = "Bad request"),
+    ApiResponse(responseCode = "401", description = "Unauthorized"),
+    ApiResponse(responseCode = "500", description = "Internal server error")
+  ])
+  @ResponseStatus(HttpStatus.CREATED)
+  @GetMapping(value = ["/ping"])
+  suspend fun ping(): String = Mono.just("pong").awaitSingle()
 }
